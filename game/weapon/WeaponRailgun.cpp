86750f507432f427e0ccd8ed41596013c0eee5ff
toshi <toshiryes21@gmail.com>
2026-03-09 20:08:19 -0400

    minor weapon tweaks and starting to try and add gladiator shield to other enemies

--- game/weapon/WeaponRailgun.cpp
+++ game/weapon/WeaponRailgun.cpp
@@ -9,34 +9,44 @@ const idEventDef EV_Railgun_RestoreHum( "<railgunRestoreHum>", "" );
 class rvWeaponRailgun : public rvWeapon {
 public:
 
-	CLASS_PROTOTYPE( rvWeaponRailgun );
+	CLASS_PROTOTYPE(rvWeaponRailgun);
 
-	rvWeaponRailgun ( void );
+	rvWeaponRailgun(void);
 
-	virtual void			Spawn				( void );
-	virtual void			Think				( void );
-	void					Save				( idSaveGame *savefile ) const;
-	void					Restore				( idRestoreGame *savefile );
-	void					PreSave				( void );
-	void					PostSave			( void );
-	void					ClientUnstale		( void );
+	virtual void		Spawn(void);
+	virtual void		Think(void);
+	void				Save(idSaveGame* savefile) const;
+	void				Restore(idRestoreGame* savefile);
+	void				PreSave(void);
+	void				PostSave(void);
+	void				ClientUnstale(void);
 
 protected:
-	jointHandle_t			jointBatteryView;
+
+	bool				UpdateAttack(void);
+	bool				UpdateFlashlight(void);
+	void				Flashlight(bool on);
 
 private:
 
-	stateResult_t		State_Idle		( const stateParms_t& parms );
-	stateResult_t		State_Fire		( const stateParms_t& parms );
-	stateResult_t		State_Reload	( const stateParms_t& parms );
+	int					chargeTime;
+	int					chargeDelay;
+	idVec2				chargeGlow;
+	bool				fireForced;
+	int					fireHeldTime;
 
-	void				Event_RestoreHum	( void );
-
-	CLASS_STATES_PROTOTYPE ( rvWeaponRailgun );
+	stateResult_t		State_Raise(const stateParms_t& parms);
+	stateResult_t		State_Lower(const stateParms_t& parms);
+	stateResult_t		State_Idle(const stateParms_t& parms);
+	stateResult_t		State_Charge(const stateParms_t& parms);
+	stateResult_t		State_Charged(const stateParms_t& parms);
+	stateResult_t		State_Fire(const stateParms_t& parms);
+	stateResult_t		State_Reload(const stateParms_t& parms);
+	void				Event_RestoreHum(void);
+	CLASS_STATES_PROTOTYPE(rvWeaponRailgun);
 };
 
-CLASS_DECLARATION( rvWeapon, rvWeaponRailgun )
-	EVENT( EV_Railgun_RestoreHum,			rvWeaponRailgun::Event_RestoreHum )
+CLASS_DECLARATION(rvWeapon, rvWeaponRailgun)
 END_CLASS
 
 /*
@@ -44,7 +54,95 @@ END_CLASS
 rvWeaponRailgun::rvWeaponRailgun
 ================
 */
-rvWeaponRailgun::rvWeaponRailgun ( void ) {
+rvWeaponRailgun::rvWeaponRailgun(void) {
+}
+
+/*
+================
+rvWeaponRailgun::UpdateFlashlight
+================
+*/
+bool rvWeaponRailgun::UpdateFlashlight(void) {
+	if (!wsfl.flashlight) {
+		return false;
+	}
+
+	SetState("Flashlight", 0);
+	return true;
+}
+
+/*
+================
+rvWeaponRailgun::Flashlight
+================
+*/
+void rvWeaponRailgun::Flashlight(bool on) {
+	owner->Flashlight(on);
+
+	if (on) {
+		worldModel->ShowSurface("models/weapons/blaster/flare");
+		viewModel->ShowSurface("models/weapons/blaster/flare");
+	}
+	else {
+		worldModel->HideSurface("models/weapons/blaster/flare");
+		viewModel->HideSurface("models/weapons/blaster/flare");
+	}
+}
+
+/*
+================
+rvWeaponRailgun::UpdateAttack
+================
+*/
+bool rvWeaponRailgun::UpdateAttack(void) {
+	// Clear fire forced
+	if (fireForced) {
+		if (!wsfl.attack) {
+			fireForced = false;
+		}
+		else {
+			return false;
+		}
+	}
+
+	// If the player is pressing the fire button and they have enough ammo for a shot
+	// then start the shooting process.
+	if (wsfl.attack && gameLocal.time >= nextAttackTime) {
+		// Save the time which the fire button was pressed
+		if (fireHeldTime == 0) {
+			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier(PMOD_FIRERATE));
+			fireHeldTime = gameLocal.time;
+			//viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, chargeGlow[0]);
+		}
+	}
+
+	// If they have the charge mod and they have overcome the initial charge 
+	// delay then transition to the charge state.
+	if (fireHeldTime != 0) {
+		if (gameLocal.time - fireHeldTime > chargeDelay) {
+			SetState("Charge", 4);
+			return true;
+		}
+
+		// If the fire button was let go but was pressed at one point then 
+		// release the shot.
+		if (!wsfl.attack) {
+			idPlayer* player = gameLocal.GetLocalPlayer();
+			if (player) {
+
+				if (player->GuiActive()) {
+					//make sure the player isn't looking at a gui first
+					SetState("Lower", 0);
+				}
+				else {
+					SetState("Fire", 0);
+				}
+			}
+			return true;
+		}
+	}
+
+	return false;
 }
 
 /*
@@ -52,8 +150,18 @@ rvWeaponRailgun::rvWeaponRailgun ( void ) {
 rvWeaponRailgun::Spawn
 ================
 */
-void rvWeaponRailgun::Spawn ( void ) {
-	SetState ( "Raise", 0 );	
+void rvWeaponRailgun::Spawn(void) {
+	//viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, 0);
+	SetState("Raise", 0);
+
+	chargeGlow = spawnArgs.GetVec2("chargeGlow");
+	chargeTime = SEC2MS(spawnArgs.GetFloat("chargeTime"));
+	chargeDelay = SEC2MS(spawnArgs.GetFloat("chargeDelay"));
+
+	fireHeldTime = 0;
+	fireForced = false;
+
+	Flashlight(owner->IsFlashlightOn());
 }
 
 /*
@@ -61,8 +169,12 @@ void rvWeaponRailgun::Spawn ( void ) {
 rvWeaponRailgun::Save
 ================
 */
-void rvWeaponRailgun::Save ( idSaveGame *savefile ) const {
-	savefile->WriteJoint( jointBatteryView );
+void rvWeaponRailgun::Save(idSaveGame* savefile) const {
+	savefile->WriteInt(chargeTime);
+	savefile->WriteInt(chargeDelay);
+	savefile->WriteVec2(chargeGlow);
+	savefile->WriteBool(fireForced);
+	savefile->WriteInt(fireHeldTime);
 }
 
 /*
@@ -70,8 +182,12 @@ void rvWeaponRailgun::Save ( idSaveGame *savefile ) const {
 rvWeaponRailgun::Restore
 ================
 */
-void rvWeaponRailgun::Restore ( idRestoreGame *savefile ) {
-	savefile->ReadJoint( jointBatteryView );
+void rvWeaponRailgun::Restore(idRestoreGame* savefile) {
+	savefile->ReadInt(chargeTime);
+	savefile->ReadInt(chargeDelay);
+	savefile->ReadVec2(chargeGlow);
+	savefile->ReadBool(fireForced);
+	savefile->ReadInt(fireHeldTime);
 }
 
 /*
@@ -79,10 +195,15 @@ void rvWeaponRailgun::Restore ( idRestoreGame *savefile ) {
 rvWeaponRailgun::PreSave
 ================
 */
-void rvWeaponRailgun::PreSave ( void ) {
+void rvWeaponRailgun::PreSave(void) {
 
-	//this should shoosh the humming but not the shooting sound.
-	StopSound( SND_CHANNEL_BODY2, 0);
+	SetState("Idle", 4);
+
+	StopSound(SND_CHANNEL_WEAPON, 0);
+	StopSound(SND_CHANNEL_BODY, 0);
+	StopSound(SND_CHANNEL_ITEM, 0);
+	StopSound(SND_CHANNEL_ANY, false);
+
 }
 
 /*
@@ -90,10 +211,7 @@ void rvWeaponRailgun::PreSave ( void ) {
 rvWeaponRailgun::PostSave
 ================
 */
-void rvWeaponRailgun::PostSave ( void ) {
-
-	//restore the humming
-	PostEventMS( &EV_Railgun_RestoreHum, 10);
+void rvWeaponRailgun::PostSave(void) {
 }
 
 /*
@@ -101,75 +219,198 @@ void rvWeaponRailgun::PostSave ( void ) {
 rvWeaponRailgun::Think
 ================
 */
-void rvWeaponRailgun::Think ( void ) {
+void rvWeaponRailgun::Think(void) {
 
 	// Let the real weapon think first
-	rvWeapon::Think ( );
+	rvWeapon::Think();
 
-	if ( zoomGui && wsfl.zoom && !gameLocal.isMultiplayer ) {
+	if (zoomGui && wsfl.zoom && !gameLocal.isMultiplayer) {
 		int ammo = AmmoInClip();
-		if ( ammo >= 0 ) {
-			zoomGui->SetStateInt( "player_ammo", ammo );
-		}			
+		if (ammo >= 0) {
+			zoomGui->SetStateInt("player_ammo", ammo);
+		}
 	}
 }
 
 /*
 ===============================================================================
 
-	States 
+	States
 
 ===============================================================================
 */
 
-CLASS_STATES_DECLARATION ( rvWeaponRailgun )
-	STATE ( "Idle",				rvWeaponRailgun::State_Idle)
-	STATE ( "Fire",				rvWeaponRailgun::State_Fire )
-	STATE ( "Reload",			rvWeaponRailgun::State_Reload )
+CLASS_STATES_DECLARATION(rvWeaponRailgun)
+STATE("Raise", rvWeaponRailgun::State_Raise)
+STATE("Lower", rvWeaponRailgun::State_Lower)
+STATE("Idle", rvWeaponRailgun::State_Idle)
+STATE("Charge", rvWeaponRailgun::State_Charge)
+STATE("Charged", rvWeaponRailgun::State_Charged)
+STATE("Fire", rvWeaponRailgun::State_Fire)
+
 END_CLASS_STATES
+
+/*
+================
+rvWeaponRailgun::State_Raise
+================
+*/
+stateResult_t rvWeaponRailgun::State_Raise(const stateParms_t& parms) {
+	enum {
+		RAISE_INIT,
+		RAISE_WAIT,
+	};
+	switch (parms.stage) {
+	case RAISE_INIT:
+		SetStatus(WP_RISING);
+		PlayAnim(ANIMCHANNEL_ALL, "raise", parms.blendFrames);
+		return SRESULT_STAGE(RAISE_WAIT);
+
+	case RAISE_WAIT:
+		if (AnimDone(ANIMCHANNEL_ALL, 4)) {
+			SetState("Idle", 4);
+			return SRESULT_DONE;
+		}
+		if (wsfl.lowerWeapon) {
+			SetState("Lower", 4);
+			return SRESULT_DONE;
+		}
+		return SRESULT_WAIT;
+	}
+	return SRESULT_ERROR;
+}
+
+/*
+================
+rvWeaponRailgun::State_Lower
+================
+*/
+stateResult_t rvWeaponRailgun::State_Lower(const stateParms_t& parms) {
+	enum {
+		LOWER_INIT,
+		LOWER_WAIT,
+		LOWER_WAITRAISE
+	};
+	switch (parms.stage) {
+	case LOWER_INIT:
+		SetStatus(WP_LOWERING);
+		PlayAnim(ANIMCHANNEL_ALL, "putaway", parms.blendFrames);
+		return SRESULT_STAGE(LOWER_WAIT);
+
+	case LOWER_WAIT:
+		if (AnimDone(ANIMCHANNEL_ALL, 0)) {
+			SetStatus(WP_HOLSTERED);
+			return SRESULT_STAGE(LOWER_WAITRAISE);
+		}
+		return SRESULT_WAIT;
+
+	case LOWER_WAITRAISE:
+		if (wsfl.raiseWeapon) {
+			SetState("Raise", 0);
+			return SRESULT_DONE;
+		}
+		return SRESULT_WAIT;
+	}
+	return SRESULT_ERROR;
+}
 
 /*
 ================
 rvWeaponRailgun::State_Idle
 ================
 */
-stateResult_t rvWeaponRailgun::State_Idle( const stateParms_t& parms ) {
+stateResult_t rvWeaponRailgun::State_Idle(const stateParms_t& parms) {
 	enum {
-		STAGE_INIT,
-		STAGE_WAIT,
-	};	
-	switch ( parms.stage ) {
-		case STAGE_INIT:
-			if ( !AmmoAvailable ( ) ) {
-				SetStatus ( WP_OUTOFAMMO );
-			} else {
-				StopSound( SND_CHANNEL_BODY2, false );
-				StartSound( "snd_idle_hum", SND_CHANNEL_BODY2, 0, false, NULL );
-				SetStatus ( WP_READY );
-			}
-			PlayCycle( ANIMCHANNEL_ALL, "idle", parms.blendFrames );
-			return SRESULT_STAGE ( STAGE_WAIT );
-		
-		case STAGE_WAIT:			
-			if ( wsfl.lowerWeapon ) {
-				StopSound( SND_CHANNEL_BODY2, false );
-				SetState ( "Lower", 4 );
+		IDLE_INIT,
+		IDLE_WAIT,
+	};
+	switch (parms.stage) {
+	case IDLE_INIT:
+		SetStatus(WP_READY);
+		PlayCycle(ANIMCHANNEL_ALL, "idle", parms.blendFrames);
+		return SRESULT_STAGE(IDLE_WAIT);
+
+	case IDLE_WAIT:
+		if (wsfl.lowerWeapon) {
+			SetState("Lower", 4);
+			return SRESULT_DONE;
+		}
+
+		if (UpdateFlashlight()) {
+			return SRESULT_DONE;
+		}
+		if (UpdateAttack()) {
+			return SRESULT_DONE;
+		}
+		return SRESULT_WAIT;
+	}
+	return SRESULT_ERROR;
+}
+
+/*
+================
+rvWeaponRailgun::State_Charge
+================
+*/
+stateResult_t rvWeaponRailgun::State_Charge(const stateParms_t& parms) {
+	enum {
+		CHARGE_INIT,
+		CHARGE_WAIT,
+	};
+	switch (parms.stage) {
+	case CHARGE_INIT:
+		//viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, chargeGlow[0]);
+		StartSound("snd_charge", SND_CHANNEL_ITEM, 0, false, NULL);
+		PlayCycle(ANIMCHANNEL_ALL, "charging", parms.blendFrames);
+		return SRESULT_STAGE(CHARGE_WAIT);
+
+	case CHARGE_WAIT:
+		if (gameLocal.time - fireHeldTime < chargeTime) {
+			float f;
+			f = (float)(gameLocal.time - fireHeldTime) / (float)chargeTime;
+			f = chargeGlow[0] + f * (chargeGlow[1] - chargeGlow[0]);
+			f = idMath::ClampFloat(chargeGlow[0], chargeGlow[1], f);
+			//viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, f);
+
+			if (!wsfl.attack) {
+				SetState("Fire", 0);
 				return SRESULT_DONE;
-			}		
-			if ( gameLocal.time > nextAttackTime && wsfl.attack && AmmoInClip ( ) ) {
-				SetState ( "Fire", 0 );
-				return SRESULT_DONE;
-			}  
-			// Auto reload?
-			if ( AutoReload() && !AmmoInClip ( ) && AmmoAvailable () ) {
-				SetState ( "reload", 2 );
-				return SRESULT_DONE;
 			}
-			if ( wsfl.netReload || (wsfl.reload && AmmoInClip() < ClipSize() && AmmoAvailable()>AmmoInClip()) ) {
-				SetState ( "Reload", 4 );
-				return SRESULT_DONE;			
-			}
+
 			return SRESULT_WAIT;
+		}
+		SetState("Charged", 4);
+		return SRESULT_DONE;
+	}
+	return SRESULT_ERROR;
+}
+
+/*
+================
+rvWeaponRailgun::State_Charged
+================
+*/
+stateResult_t rvWeaponRailgun::State_Charged(const stateParms_t& parms) {
+	enum {
+		CHARGED_INIT,
+		CHARGED_WAIT,
+	};
+	switch (parms.stage) {
+	case CHARGED_INIT:
+		//viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, 1.0f);
+
+		StopSound(SND_CHANNEL_ITEM, false);
+		StartSound("snd_charge_loop", SND_CHANNEL_ITEM, 0, false, NULL);
+		StartSound("snd_charge_click", SND_CHANNEL_BODY, 0, false, NULL);
+		return SRESULT_STAGE(CHARGED_WAIT);
+
+	case CHARGED_WAIT:
+		if (!wsfl.attack) {
+			fireForced = true;
+			SetState("Fire", 0);
+			return SRESULT_DONE;
+		}
+		return SRESULT_WAIT;
 	}
 	return SRESULT_ERROR;
 }
@@ -179,25 +420,62 @@ stateResult_t rvWeaponRailgun::State_Idle( const stateParms_t& parms ) {
 rvWeaponRailgun::State_Fire
 ================
 */
-stateResult_t rvWeaponRailgun::State_Fire ( const stateParms_t& parms ) {
+stateResult_t rvWeaponRailgun::State_Fire(const stateParms_t& parms) {
 	enum {
-		STAGE_INIT,
-		STAGE_WAIT,
-	};	
-	switch ( parms.stage ) {
-		case STAGE_INIT:
-			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-			Attack ( false, 1, spread, 0, 1.0f );
-			PlayAnim ( ANIMCHANNEL_ALL, "fire", 0 );	
-			return SRESULT_STAGE ( STAGE_WAIT );
-	
-		case STAGE_WAIT:		
-			if ( ( gameLocal.isMultiplayer && gameLocal.time >= nextAttackTime ) || 
-				 ( !gameLocal.isMultiplayer && ( AnimDone ( ANIMCHANNEL_ALL, 2 ) ) ) ) {
-				SetState ( "Idle", 0 );
-				return SRESULT_DONE;
-			}		
-			return SRESULT_WAIT;
+		FIRE_INIT,
+		FIRE_WAIT,
+	};
+	switch (parms.stage) {
+	case FIRE_INIT:
+
+		//StopSound(SND_CHANNEL_ITEM, false);
+		//viewModel->SetShaderParm(BLASTER_SPARM_CHARGEGLOW, 0);
+		//don't fire if we're targeting a gui.
+		idPlayer* player;
+		player = gameLocal.GetLocalPlayer();
+
+		//make sure the player isn't looking at a gui first
+		if (player && player->GuiActive()) {
+			fireHeldTime = 0;
+			SetState("Lower", 0);
+			return SRESULT_DONE;
+		}
+
+		if (player && !player->CanFire()) {
+			fireHeldTime = 0;
+			SetState("Idle", 4);
+			return SRESULT_DONE;
+		}
+
+
+
+		if (gameLocal.time - fireHeldTime > chargeTime) {
+
+			Attack(false, 1, spread, 0, 1.0f);
+			PlayEffect("fx_chargedflash", barrelJointView, false);
+			PlayAnim(ANIMCHANNEL_ALL, "chargedfire", parms.blendFrames);
+			
+
+		}
+		else {
+			Attack(false, 1, spread, 0, 0.2f);
+			PlayEffect("fx_normalflash", barrelJointView, false);
+			PlayAnim(ANIMCHANNEL_ALL, "fire", parms.blendFrames);
+		}
+		fireHeldTime = 0;
+		
+
+		return SRESULT_STAGE(FIRE_WAIT);
+
+	case FIRE_WAIT:
+		if (AnimDone(ANIMCHANNEL_ALL, 4)) {
+			SetState("Idle", 4);
+			return SRESULT_DONE;
+		}
+		if (UpdateFlashlight() || UpdateAttack()) {
+			return SRESULT_DONE;
+		}
+		return SRESULT_WAIT;
 	}
 	return SRESULT_ERROR;
 }
